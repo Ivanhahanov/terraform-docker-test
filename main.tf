3640ed8f2ceaa87b40878d2293bc846b00a51f26
Ivan Khakhanov <ikhakhanov@ptsecurity.com>
2023-06-09 21:53:50 +0300

    add suffix

--- main.tf
+++ main.tf
@@ -23,8 +23,14 @@ resource "docker_image" "nginx" {
 
 # Create a docker container resource
 # -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
+resource "random_string" "random_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "docker_container" "nginx" {
-  name    = "nginx"
+  name    = "nginx-${random_string.random_suffix}"
   image   = docker_image.nginx.image_id
 
   ports {
